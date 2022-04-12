261205fa0a4a980cd5d74193d119480906b07385
QaTajwar <103096244+QaTajwar@users.noreply.github.com>
2022-04-12 18:25:40 -0400

    Java Switch

---
+++ src/switch1/Switch.java
@@ -0,0 +1,52 @@
+package switch1;
+
+public class Switch {
+	public static void main (String []args) {
+		
+		String day = "pizza";
+		switch (day) {
+		
+		case  "sunday" : System.out.println("It is sunday");
+		break;
+	
+		case  "monday" : System.out.println("It is monday");
+		break;
+		
+		case  "tuesday" : System.out.println("It is tuesday");
+		break;
+		
+		case  "wednesday" : System.out.println("It is wednesday");
+		break;
+	
+		case  "thursday" : System.out.println("It is thursday");
+		break;
+		
+		case  "friday" : System.out.println("It is friday");
+		break;
+		
+		case  "saturday" : System.out.println("It is saturday");
+		break;
+		
+		default : System.out.println("This is not a day");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+		
+		
+		
+		
+	}
+
+	
+	
+	
+}
